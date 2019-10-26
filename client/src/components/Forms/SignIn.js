51a4e36f6d883ddac0bd30b751846fd3553a6af2
Meguel <chavezmeguel@gmail.com>
2019-10-25 22:25:30 -0400

    add landing and sign in components

---
+++ client/src/components/Forms/SignIn.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
+const SignIn = (props) => {
+    return (
+        <Form >
+            <FormGroup>
+                <Label for="exampleEmail" >Email</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+            </FormGroup>
+            {' '}
+            <FormGroup>
+                <Label for="examplePassword" >Password</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="Password" />
+            </FormGroup>
+            {' '}
+            <div class="d-flex justify-content-end">
+                <Button>Submit</Button>
+            </div>
+        </Form>
+    );
+}
+
+export default SignIn;
---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { Container, TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
+import SignIn from '../Forms/SignIn';
+
+const Landing = (props) => {
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = tab => {
+        if (activeTab !== tab) setActiveTab(tab);
+    }
+    const navStyle = { cursor: "default" }
+
+    return (
+        <Container>
+            <Nav tabs>
+                <NavItem>
+                    <NavLink
+                        className={classnames({ active: activeTab === '1' })}
+                        onClick={() => { toggle('1'); }}
+                        style={navStyle}
+                    >
+                        Sign In
+          </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+                        className={classnames({ active: activeTab === '2' })}
+                        onClick={() => { toggle('2'); }}
+                        style={navStyle}
+                    >
+                        Register
+          </NavLink>
+                </NavItem>
+            </Nav>
+            <TabContent activeTab={activeTab}>
+                <TabPane tabId="1">
+                    <Container>
+                        <Row>
+                            <Col sm="12" md="6">
+                                <SignIn />
+                            </Col>
+                        </Row>
+                    </Container>
+                </TabPane>
+                <TabPane tabId="2">
+                    <Row>
+                        <Col sm="6">
+                            <Card body>
+                                <CardTitle>Special Title Treatment</CardTitle>
+                                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                                <Button>Go somewhere</Button>
+                            </Card>
+                        </Col>
+                        <Col sm="6">
+                            <Card body>
+                                <CardTitle>Special Title Treatment</CardTitle>
+                                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                                <Button>Go somewhere</Button>
+                            </Card>
+                        </Col>
+                    </Row>
+                </TabPane>
+            </TabContent>
+        </Container>
+    );
+}
+
+export default Landing;
